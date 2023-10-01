c62920e17991c86caf981f01f0304f9483f0de4d
Kolby Kappes <kolbykappes@gmail.com>
2023-10-01 11:37:41 -0400

    Updates to styling and buttons

--- miss-k-sentence-slam/src/app/app.component.ts
+++ miss-k-sentence-slam/src/app/app.component.ts
@@ -19,7 +19,11 @@ export class AppComponent implements OnInit {
   currentQuestionIndex = 0;
   score = 0;
   currentLevel = 1;
-  showCorrectAnswer = false;  // New property to control the display of the correct answer
+  showCorrectAnswer = false;
+  remainingTime: number | null = null;
+  overlayMessage: string | null = null;
+  buttonsDisabled = false;
+  feedbackIcons: string[] = [];
 
   constructor(private questionService: QuestionService) { }
 
@@ -32,21 +36,46 @@ export class AppComponent implements OnInit {
     this.questions = this.questionService.getQuestions().filter(q => q.level === level);
     this.currentQuestionIndex = 0;
     this.score = 0;
-    this.showCorrectAnswer = false;  // Reset for a new game
+    this.showCorrectAnswer = false;
+    this.startTimer();
+  }
+
+  startTimer() {
+    if (this.questions[this.currentQuestionIndex].timeLimit) {
+      this.remainingTime = this.questions[this.currentQuestionIndex].timeLimit;
+      const timer = setInterval(() => {
+        if (this.remainingTime! > 0) {
+          this.remainingTime!--;
+        } else {
+          clearInterval(timer);
+          this.nextQuestion();
+        }
+      }, 1000);
+    }
   }
 
   checkAnswer(selectedIndex: number, correctAnswers: number[]) {
+    this.buttonsDisabled = true;
     if (correctAnswers.includes(selectedIndex)) {
       this.score++;
-      this.showCorrectAnswer = false;  // Hide the correct answer
-      this.nextQuestion();
+      this.overlayMessage = "You're right!";
+      this.feedbackIcons[selectedIndex] = 'check';
     } else {
-      this.showCorrectAnswer = true;  // Show the correct answer
+      this.overlayMessage = "Sorry, that's not it";
+      this.feedbackIcons[selectedIndex] = 'close';
+      this.showCorrectAnswer = true;
     }
+    setTimeout(() => {
+      this.overlayMessage = null;
+      this.nextQuestion();
+    }, 2000);
   }
 
   nextQuestion() {
     this.currentQuestionIndex++;
-    this.showCorrectAnswer = false;  // Hide the correct answer for the next question
+    this.showCorrectAnswer = false;
+    this.buttonsDisabled = false;
+    this.feedbackIcons = [];
+    this.startTimer();
   }
 }
